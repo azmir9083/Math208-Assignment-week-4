a23cb8389ccfc4f6a3df2489d03395485fafd9e4
azmir9083 <146428076+azmir9083@users.noreply.github.com>
2023-12-07 23:26:26 -0800

    updte the value of binomial and normal probability

--- 19985_md_azmir_bhuiyan_question_number_4.py
+++ 19985_md_azmir_bhuiyan_question_number_4.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import norm
 
 n = 100
 p = 0.1
@@ -11,6 +12,12 @@ mean = n * p
 std = np.sqrt(n * p * q)
 
 normal_data = np.random.normal(mean, std, 10000)
+binomial_prob = np.sum(binomial_data == 10) / len(binomial_data)
+
+normal_prob = norm.cdf(10, loc=mean, scale=std) - norm.cdf(9, loc=mean, scale=std)
+
+print("Binomial Probability (X = 10):", binomial_prob)
+print("Normal Probability (X = 10):", normal_prob)
 
 plt.hist(binomial_data, bins=30, alpha=0.5, label='Binomial', color='blue')
 plt.hist(normal_data, bins=30, alpha=0.5, label='Normal', color='red')
@@ -19,3 +26,5 @@ plt.xlabel('Value')
 plt.ylabel('Frequency')
 plt.legend()
 plt.show()
+
+
